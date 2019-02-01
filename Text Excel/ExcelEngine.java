d23c79d625c9d1e25acba537c915929e8aa4fd6d
Nick Erb <44888363+Tenebrosi-Erinys@users.noreply.github.com>
2019-02-01 08:44:52 -0800

    Add files via upload

--- Text Excel/ExcelEngine.java
+++ Text Excel/ExcelEngine.java
@@ -1,3 +1,6 @@
+import java.util.*;
+import java.io.*;
+
 public class ExcelEngine extends ExcelBase {
    public static void main(String args[]){
       GridBase.grid = new Grid();
@@ -6,7 +9,27 @@ public class ExcelEngine extends ExcelBase {
       engine.runInputLoop();
    }
    public String processCommand(String input){
+      if(input.contains("load")){
+         try{
+            return loadFile(input);
+         } 
+         catch (FileNotFoundException e){
+            return "File loading failed";
+         }
+      }
+      
       
       return GridBase.grid.processCommand(input);
+   }
+   public String loadFile(String input) throws FileNotFoundException {
+      Scanner parse = new Scanner(input);
+      parse.next();
+      File file = new File(parse.next());
+      Scanner fileP = new Scanner(file);
+      
+      while(fileP.hasNextLine()){
+         GridBase.grid.processCommand(fileP.nextLine());
+      }
+      return "File loaded successfully";
    }
 }
--- Text Excel/Grid.java
+++ Text Excel/Grid.java
@@ -7,21 +7,52 @@ public class Grid extends GridBase{
    public int width = 9;
    
    public String processCommand(String input){
-      if(input.equalsIgnoreCase("rows")){
-         return rows + "";
-      } 
-      else if(input.equalsIgnoreCase("cols")){
-         return cols + "";
-      } 
-      else if(input.equalsIgnoreCase("width")){
-         return width + "";
+      if(input.contains("rows") || input.contains("cols") || input.contains("width")){
+         return varController(input);
       }
-      
-      if(input.equalsIgnoreCase("print")){
+      else if(input.equalsIgnoreCase("print")){
          return printGrid();
       }
    
       return input;
+   }
+   
+   public String varController(String input){
+      boolean isRows = false;
+      boolean isCols = false;
+      boolean isWidth = false;
+      Scanner parser = new Scanner(input);
+      String token = parser.next();
+      if(token.equalsIgnoreCase("rows")){
+         isRows = true;
+      } 
+      else if(token.equalsIgnoreCase("cols")){
+         isCols = true;
+      } 
+      else if(token.equalsIgnoreCase("width")){
+         isWidth = true;
+      }
+      if(parser.hasNext()){
+         token = parser.next();
+      }
+      int changeVar = 0;
+      if(token.equalsIgnoreCase("=") && parser.hasNext()){
+         changeVar = Integer.parseInt(parser.next());
+      } else if(isRows){
+         return rows + "";
+      } else if(isCols){
+         return cols + "";
+      } else if(isWidth){
+         return width + "";
+      }
+      if(isRows){
+         rows = changeVar;
+      } else if(isCols){
+         cols = changeVar;
+      } else if(isWidth){
+         width = changeVar;
+      }
+      return changeVar + "";
    }
    
    public String printGrid(){
@@ -29,13 +60,13 @@ public class Grid extends GridBase{
       char currentLetter = 'A';
       int rightSpaces = width / 2;
       for(int currentRows = 0; currentRows <= rows; currentRows++){
-         int digits = digitsInNum(currentRows);
          if(currentRows != 0){
-            gridString += charSomeTimes(' ', (rightSpaces - digits) / 2 + digits % 2);
-            gridString += currentRows;
-            gridString += charSomeTimes(' ', rightSpaces / 2 - 1);
-         } else {
-            gridString += charSomeTimes(' ', rightSpaces);
+            gridString += charSomeTimes(' ', 1);
+            gridString += String.format("%2d", currentRows);
+            gridString += charSomeTimes(' ', 1);
+         } 
+         else {
+            gridString += charSomeTimes(' ', 4);
          }
          gridString += "|";
          for(int columns = 0; columns < cols; columns++){
@@ -43,16 +74,17 @@ public class Grid extends GridBase{
             if(currentRows == 0){
                gridString += currentLetter;
                currentLetter += 1;
-            } else {
+            } 
+            else {
                gridString += " ";
                
             }
-            gridString += charSomeTimes(' ', rightSpaces);
+            gridString += charSomeTimes(' ', rightSpaces - (width + 1) % 2);
             gridString += "|";
          
          }
          gridString += "\n";
-         gridString += longLine(rightSpaces);
+         gridString += longLine();
          gridString += "\n";
       }
       return gridString;
@@ -65,9 +97,9 @@ public class Grid extends GridBase{
       }
       return string;
    }
-   public String longLine(int rightSpaces){
+   public String longLine(){
       String line = "";
-      line += charSomeTimes('-', rightSpaces);
+      line += charSomeTimes('-', 4);
       line += "+";
       for(int columns = 0; columns < cols; columns++){
          line += charSomeTimes('-', width);
